e1851028f40544cee14d5f975eaaa16226641a44
Rikstone <tarikbouari@gmail.com>
2022-08-02 07:34:52 +0000

    add data to localstorage

--- data.js
+++ data.js
@@ -1,47 +1,61 @@
+const data = [];
 
-data = [ { title: "The life secret",
-            Author: "Andreas Pick"
-
-        }]
-
-
-const add = document.getElementById('add');
+// const add = document.getElementById('add');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const form = document.getElementById('form');
 const message = document.getElementById('message');
+const container = document.getElementById('container');
+const btn = document.querySelectorAll('.btn');
+console.log(btn);
 
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  //const inputTitle = title.value;
+  //const inputAuthor = author.value;
 
+  const bookData = {
+    title: title.value,
+    author: author.value,
+  };
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-
-    let bookData = {
-        title : title.value,
-        author : author.value
-    }
-
-    localStorage.setItem('user', JSON.stringify(bookData));
-
-    add.addEventListener('onclick', (e)=>{
-        
-        if (bookData.title & bookData.author){
-        form.submit();
-        form.reset();
-        data.push(bookData);
-    }
-    else {
-        message.textContent = ' Title and Author are required'
-        return "don't exit";
-    }
-
-    })
-        
-    
-
-    
   
+  function addBook() {
+    const load = ` <p> ${bookData.title}</p>
+  <p>${bookData.author}</p>
+  <button class="btn"> Remove</button> 
+  <hr>`;
+    container.innerHTML += load;
+  }
 
-})
+  if ((bookData.title) && (bookData.author)) {
+   //  form.submit();
+    data.push(bookData);
+    addBook();
+    localStorage.setItem('user', JSON.stringify(bookData));
+    form.reset();
+  } else {
+    message.textContent = 'Title and Author are required';
+  }
+  return data;
+
+
+});
+
+// function removeBook() {
+//  btn.forEach( item => {
+//   item.parentElement.style.display="none";
+//  });
+   
+// }
+
+btn.forEach( (item) => {
+  item.addEventListener('click', (e) => {
+    let currentBtn = e.currentTarget
+    currentBtn.parentElement.style.display="none";
+    console.log('hello');
+  })
+  
+  })
 
