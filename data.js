4cf871b48c6a2fbc999079b4c5984fbd1186da8e
Rikstone <tarikbouari@gmail.com>
2022-08-02 09:56:33 +0000

    fetch data from localstorage

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const data = [];
+let data = [ {title: "title" , author: "author"}];
 
 // const add = document.getElementById('add');
 const title = document.getElementById('title');
@@ -9,6 +9,32 @@ const container = document.getElementById('container');
 const btn = document.querySelectorAll('.btn');
 console.log(btn);
 
+ getData = localStorage.getItem('userdata');
+
+if(!getData){
+  data.forEach(item => {
+    
+   let initialize = ` <p> ${item.title}</p>
+  <p>${item.author}</p>
+  <button class="btn">Remove</button> 
+  <hr> `;
+  container.innerHTML += initialize;
+
+  });
+  
+} else if (getData) {
+  let dataStorage = JSON.parse(getData);
+
+  dataStorage.forEach(item => {
+
+    let initialize = ` <p> ${item.title}</p>
+    <p>${item.author}</p>
+    <button class="btn">Remove</button> 
+    <hr> `;
+    container.innerHTML += initialize;  
+  })
+
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -20,7 +46,6 @@ form.addEventListener('submit', (e) => {
     author: author.value,
   };
 
-  
   function addBook() {
     const load = ` <p> ${bookData.title}</p>
   <p>${bookData.author}</p>
@@ -28,17 +53,17 @@ form.addEventListener('submit', (e) => {
   <hr>`;
     container.innerHTML += load;
   }
-
   if ((bookData.title) && (bookData.author)) {
    //  form.submit();
     data.push(bookData);
+    console.log(data);
     addBook();
-    localStorage.setItem('user', JSON.stringify(bookData));
-    form.reset();
+    localStorage.setItem('userdata', JSON.stringify(data));
+    // form.reset();
   } else {
     message.textContent = 'Title and Author are required';
   }
-  return data;
+  console.log(data);
 
 
 });
