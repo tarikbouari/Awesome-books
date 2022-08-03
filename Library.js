058148f129cb7d197e1ee1fb7b7e76cadc6ffbbb
Rikstone <tarikbouari@gmail.com>
2022-08-03 14:09:59 +0000

    add addBook method

--- Library.js
+++ Library.js
@@ -4,18 +4,19 @@ class Library {
         this.title = title;
         this.author = author;
         this.id = Math.random() * 10;
-        // this.data = [];
     }
+    
     
     checkData(){
         getData = localStorage.getItem('userdata');
      if (!getData) {
         container.innerHTML = '<h3> Add book to your colection</h3>';
       } else if (getData) {
-        let da
-        this.data = JSON.parse(getData);
+         let data =[];
+
+        data = JSON.parse(getData);
       
-        this.data.forEach((item) => {
+        data.forEach((item) => {
           const initialize = ` <div class="undo"><p> ${item.title}</p>
           <p>${item.author}</p>
           <button class="btn" id="${item.id}">Remove</button> 
@@ -26,29 +27,43 @@ class Library {
 
     }
 
-     
     addBook(){
-        
-        
+      let data=[];
+      getData = localStorage.getItem('userdata');
+       data = JSON.parse(getdata);
+
+        let obj = {
+          title: this.title,
+            author: this.author,
+            id: this.id
+        }; 
+
         if((this.title && this.author)){
-          this.data.push();
-          addBook();
-          localStorage.setItem('userdata', JSON.stringify(this.data));
+          data.push(obj);
+          
+          data.forEach((item) => {
+            const initialize = ` <div class="undo"><p> ${item.title}</p>
+            <p>${item.author}</p>
+            <button class="btn" id="${item.id}">Remove</button> 
+            <hr></div> `;
+            container.innerHTML += initialize;
+          });
+
+          localStorage.setItem('userdata', JSON.stringify(data));
           form.reset();
           window.location.reload();
         } else {
           message.textContent = 'Title and Author are required';
         }
         
-        const load = ` <div class="undo" ><p> ${this.title}</p>
-        <p>${this.author}</p>
-        <button class="btn" id="${this.id}"> Remove</button> 
-        <hr></div>`;
-          container.innerHTML += load;
-
     }
 
+    removebook(){
 
+        
+
+    } 
+    
 
 
 
