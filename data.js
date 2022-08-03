5d449c60373d0174fad8cb7caf86dcf3cc3cb9ba
Rikstone <tarikbouari@gmail.com>
2022-08-03 09:53:43 +0000

    add remove event

--- data.js
+++ data.js
@@ -1,126 +1,25 @@
-// let data = [ ];
+let data = [];
 
-// // const add = document.getElementById('add');
-// const title = document.getElementById('title');
-// const author = document.getElementById('author');
-// const form = document.getElementById('form');
-// const message = document.getElementById('message');
-// const container = document.getElementById('container');
-// const removeBtn = document.querySelectorAll('.btn');
-// // console.log(removeBtn);
-//  date = new Date();
-//  getData = localStorage.getItem('userdata');
-
-// if(!getData){
-//   // data.forEach(item => {    
-//  let initialize = `<h1> Add a book to your collection<h/> `;
-//   container.innerHTML += initialize;
-
-// } else if (getData) {
-//   let dataStorage = JSON.parse(getData);
-
-//   dataStorage.forEach(item => {
-
-//     let initialize = ` <p> ${item.title}</p>
-//     <p>${item.author}</p>
-//     <button class="btn">Remove</button> 
-//     <hr> `;
-//     container.innerHTML += initialize;  
-//   })
-
-// }
-
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   //const inputTitle = title.value;
-//   //const inputAuthor = author.value;
-//   const bookData = {
-//     title: title.value,
-//     author: author.value,
-//   };
- 
-//   function addBook() {
-//     for( i =0; i< 50; i++){
-      
-//       const load = `<p> ${bookData.title}</p>
-//       <p>${bookData.author}</p>
-//       <button class="btn" data-id = "${i++}" > Remove</button> 
-//       <hr>`;
-//         container.innerHTML += load;
-//     }
-      
-//     } 
-   
-//   if ((bookData.title) && (bookData.author)) {
-//    //  form.submit();
-//     data.push(bookData);
-//     console.log(data);
-//     addBook();
-//     localStorage.setItem('userdata', JSON.stringify(data));
-//     // form.reset();
-//   } else {
-//     message.textContent = 'Title and Author are required';
-//   }
-//   console.log(data);
-
-
-// });
-
-// // function removeBook() {
-// //  btn.forEach( item => {
-// //   item.parentElement.style.display="none";
-// //  });
-   
-// // }
-
-// removeBtn.forEach( (item) => {
-//   item.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     let currentBtn = e.currentTarget
-//     console.log('hello');
-//     currentBtn.parentElement.style.display="none";
-   
-//   })
-  
-//   })
-
-
-let data = [ {title: "title" , author: "author"}];
-
-// const add = document.getElementById('add');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const form = document.getElementById('form');
 const message = document.getElementById('message');
 const container = document.getElementById('container');
 
+const getData = localStorage.getItem('userdata');
 
- getData = localStorage.getItem('userdata');
-
-if(!getData){
-  
-  data.forEach(item => {
-    
-   let initialize = ` <div class"undo"><p> ${item.title}</p>
-  <p>${item.author}</p>
-  <button class="btn" id="${item.id}">Remove</button> 
-  <hr></div> `;
-  container.innerHTML += initialize;
-
-  });
-  
+if (!getData) {
+  container.innerHTML = '<h3> Add book to your colection</h3>';
 } else if (getData) {
-   data = JSON.parse(getData);
+  data = JSON.parse(getData);
 
-  data.forEach(item => {
-
-    let initialize = ` <div class="undo"><p> ${item.title}</p>
+  data.forEach((item) => {
+    const initialize = ` <div class="undo"><p> ${item.title}</p>
     <p>${item.author}</p>
     <button class="btn" id="${item.id}">Remove</button> 
     <hr></div> `;
-    container.innerHTML += initialize;  
-  })
-
+    container.innerHTML += initialize;
+  });
 }
 
 form.addEventListener('submit', (e) => {
@@ -128,7 +27,7 @@ form.addEventListener('submit', (e) => {
   const bookData = {
     title: title.value,
     author: author.value,
-    id: Date.now()
+    id: Date.now(),
   };
 
   function addBook() {
@@ -139,35 +38,37 @@ form.addEventListener('submit', (e) => {
     container.innerHTML += load;
   }
   if ((bookData.title) && (bookData.author)) {
-   data.push(bookData);
+    data.push(bookData);
     addBook();
     localStorage.setItem('userdata', JSON.stringify(data));
     form.reset();
-   
+    window.location.reload();
   } else {
     message.textContent = 'Title and Author are required';
   }
-  const btn = document.querySelectorAll('.btn');
-  
-  btn.forEach( (item) => {
-    console.log("hello");
-    item.addEventListener('click', (e) => {
-      if(e.target.classList.contains("btn")){
-        data = data.filter(item =>item.id != e.target.id)
-        localStorage.setItem('userdata', JSON.stringify(data));
-  
-        data.forEach(item => {
-      
-          let initialize = ` <div class"undo"><p> ${item.title}</p>
-         <p>${item.author}</p>
-         <button class="btn" id="${item.id}">Remove</button> 
-         <hr></div> `;
-         container.innerHTML += initialize;
-        
-      })
-    };
-  });
-  });
-  
 });
 
+const btn = document.querySelectorAll('.btn');
+
+btn.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    const currentBtn = e.currentTarget;
+
+    const output = data.filter((item) => {
+      if (item.id != currentBtn.id) {
+        return item
+      }
+    });
+    localStorage.setItem('userdata', JSON.stringify(output));
+    data = JSON.parse(getData);
+    data.forEach((item) => {
+      const initialize = ` <div class="undo"><p> ${item.title}</p>
+      <p>${item.author}</p>
+      <button class="btn" id="${item.id}">Remove</button> 
+      <hr></div> `;
+      container.innerHTML += initialize;
+      window.location.reload();
+    });
+  });
+});
